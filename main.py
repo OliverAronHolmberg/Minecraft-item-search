d0ca951f5497efa4c08e28115fc369d8809e0e07
Oliver Holmberg <63006762+Oliver1526@users.noreply.github.com>
2025-08-24 11:30:48 +0200

    Added container searching

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ item_id = input("Enter the Item ID you want to find, (e.g. minecraft:diamond): "
 
 
 
+print("\nSearching player Inventories")
 playerdata_path = os.path.join(world_input, "playerdata")
 
 for filename in os.listdir(playerdata_path):
@@ -39,3 +40,31 @@ for filename in os.listdir(playerdata_path):
 
         except Exception as e:
             print("Errpr reading {filename}: {e}")
+
+
+print("\nSearching World Containers (chest, barrels, etc.)")
+region_path = os.path.join(world_input, "region")
+
+for filename in os.listdir(region_path):
+    if filename.endswith(".mca"):
+        filepath = os.path.join(region_path, filename)
+        try:
+            region = anvil.Region.from_file(filepath)
+
+            for x in range(32):
+                for z in range(32):
+                    try:
+                        chunk = region.get_chunk(x, z)
+
+                        for tile in chunk.tile_entities:
+                            if "Items" in tile:
+                                for item in tile["Items"]:
+                                    if item["id"] == item_id:
+                                        coords = (tile["x"].value, tile["y"].value, tile["z"].value)
+                                        count = int(item["Count"].value)
+                                        print(f"Found {count}x {item_id} in container at {coords}")
+                    except Exception:
+                        pass
+        except Exception:
+            print("Error reading at region {filename}: {e}")
+    
