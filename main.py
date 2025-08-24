741a847966f537226a9173ea7f60eac57948b51d
Oliver Holmberg <63006762+Oliver1526@users.noreply.github.com>
2025-08-24 11:21:40 +0200

    Finished Searching player and enderchest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import nbtlib
+import anvil
 import os
 
 world_input = input("Enter the path of your minecraft world: ")
@@ -7,7 +8,34 @@ item_id = input("Enter the Item ID you want to find, (e.g. minecraft:diamond): "
 
 
 
-player_data = os.path.join(world_input, "playerdata")
+playerdata_path = os.path.join(world_input, "playerdata")
 
-for filename in os.listdir(player_data):
-    
+for filename in os.listdir(playerdata_path):
+    if filename.endswith(".dat"):
+        filepath = os.path.join(playerdata_path, filename)
+        try:
+            nbt = nbtlib.load(filepath)
+
+            #Player Position
+            pos = nbt["Pos"]
+            pos_tuple = (float(pos[0]), float(pos[1]), float(pos[2]))
+
+            found = False
+
+            for item in nbt["Inventory"]:
+                if item["id"] == item_id:
+                    found = True
+                    count = int(item["Count"])
+                    print(f"Found {count}x {item_id} in {filename}'s Inventory at {pos_tuple}")
+
+            for item in nbt.get("EnderItems", []):
+                if item["id"] == item_id:
+                    found = True
+                    count = int(item["Count"])
+                    print(f"Found {count}x {item_id} in {filename}'s Ender Cheast at {pos_tuple}")
+
+            if not found:
+                print(f"No {item_id} found in {filename}")
+
+        except Exception as e:
+            print("Errpr reading {filename}: {e}")
